41cc428be3fa734e81d0ecc5394f54c38848a67a
Rubensio <rumont98@gmail.com>
2021-12-10 18:36:28 +0100

    modelo hecho

--- resources/views/components/tablecomponent.blade.php
+++ resources/views/components/tablecomponent.blade.php
@@ -1,117 +1,41 @@
 <section class="p-5 gradient-custom">
-    <div class="card bg-light shadow p-3 m-5 rounded">
-        <div class="card-body">
-    
-            <table id="table_id" class="table table-striped table-hover table-bordered rounded">
-                <thead>
-                    <tr>
-                        <th>Email</th>
-                        <th>Entry Time</th>
-                        <th>Exit time</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td>example@email.com</td>
-                        <td>8:00 am</td>
-                        <td>5:00 pm</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+  <div class="card bg-light shadow p-3 m-5 rounded">
+    <div class="card-body">
+
+      <table id="table_id" class="table table-striped table-hover table-bordered rounded">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>DNI</th>
+            <th>Time</th>
+            <th>Entry/Exit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>0</td>
+            <td>302934829F</td>
+            <td>2038-01-19 03:14:07</td>
+            <td>Entry</td>
+          </tr>
+          {{-- @foreach ($data as $d)
+            <tr>
+              <td>{{ $d->email }}</td>
+              <td>{{ $d->entry_time }}</td>
+              <td>{{ $d->exit_time }}</td>
+            </tr>
+          @endforeach --}}
+        </tbody>
+      </table>
     </div>
+  </div>
 </section>
 <script>
-    $(document).ready(function() {
-        $('#table_id').DataTable({
-            responsive: true,
-            autoWidth: false
-        });
+  $(document).ready(function() {
+    $('#table_id').DataTable({
+      responsive: true,
+      autoWidth: false
     });
+  });
 </script>
 </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -36,14 +36,14 @@
   </style>
 </head>
 
-<body>
+<body style="background-color: lightblue">
   @include('components/navbar')
-  <section class="vh-100 gradient-custom ">
+  <section class="vh-100 gradient-custom">
     <div class="container py-5 h-100">
       <div class="row justify-content-center align-items-center h-100">
         <div class="col-12 col-lg-9 col-xl-7">
           <div class="card shadow-lg card-registration" style="border-radius: 15px;">
-            <div class="card-body p-4 p-md-5">
+            <div class="card-body p-4">
               <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Registration</h3>
               <form>
 

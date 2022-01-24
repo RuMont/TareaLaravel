3fcecac65c7c65ddacd8078b5c008dad32dc39f1
= <=>
2022-01-24 19:28:22 +0100

    users done

--- database/factories/UsersFactory.php
+++ database/factories/UsersFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Users;
+use App\Models\Centro;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UsersFactory extends Factory
@@ -19,7 +20,7 @@ class UsersFactory extends Factory
             'name' => $this->faker->name(),
             'surname' => $this->faker->name(),
             'birth_date' => $this->faker->date('Y-m-d H:i:s'),
-            'centre_id' => $this->faker->numerify(),
+            'centre_id' => Centro::all()->random()->id,
             'created_at' => $this->faker->date('Y-m-d H:i:s'),
             'updated_at' => $this->faker->date('Y-m-d H:i:s')
         ];
